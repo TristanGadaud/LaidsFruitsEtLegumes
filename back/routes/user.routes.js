9dc2cab578364939c6775f56832da9612bc9da50
Cyril Mammar <mammar.cyril@epitech.eu>
2021-11-27 20:30:49 +0100

    [ADD] /api/user/getUserbyId GET.

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import { getUser } from '../middleware/getUser.js';
 import { jwtVerify } from '../middleware/authJwt.js';
-import { getUserInfos } from '../controllers/user.controllers.js';
+import { getUserInfos, getUserInfosById } from '../controllers/user.controllers.js';
 
 export const userRoutes = Router();
 
 userRoutes.get('/', jwtVerify, getUser, getUserInfos);
+userRoutes.get('/getUserById', getUserInfosById);
